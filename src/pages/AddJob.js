a8464d2ca20d9cd6a5f53878361cc0ffc88fcd48
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-04-12 17:17:50 -0300

    luiz1

--- src/pages/AddJob.js
+++ src/pages/AddJob.js
@@ -17,6 +17,18 @@ export  default class AddJob extends React.Component {
 	onChangeServico=(event)=>{
 		this.setState({nomeServico:event.target.value})
 	}
+	onChangeDescricao=(event)=>{
+		this.setState({descricao:event.target.value})
+	}
+	onChangePreco=(event)=>{
+		this.setState({preco:event.target.value})
+	}
+	onChangePagamento=(event)=>{
+		this.setState({tipoPagamento:event.target.value})
+	}
+	onChangeCalendario=(event)=>{
+		this.setState({calendario:event.target.value})
+	}
 
 	adicionaServico=()=>{
 		const url = "{{baseURL}}/jobs"
@@ -31,13 +43,13 @@ export  default class AddJob extends React.Component {
 			dueDate:"2021-12-30"			
 		}
 
-		axios.post(url, headers, body)
+		/*axios.post(url, headers, body)
 		.then((resposta)=>{
 			alert("Serviço incluido com sucesso!")
 			this.setState({nomeServico:""})
 		}).catch((error)=>{
 			alert(err.resposta.data.message)
-		})
+		})*/
 	}
 	
 	//onChangeDescricao
@@ -55,18 +67,50 @@ export  default class AddJob extends React.Component {
 	  
 	  return (
 		<div>
-
-			<input placeholder="Digite nome do serviço" type="text" onChange={this.onChangeServico} value={this.state.nomeServico}/>
-
+		<h1>Cadastre o Seu serviço</h1>
+			<input placeholder="Digite nome do serviço" 
+			type="text" 
+			onChange={this.onChangeServico} 
+			value={this.state.nomeServico}/>
 			
-		  <p>1. Nome do serviço oferecido
+			<input placeholder="Digite a descrição do serviço" 
+			type="text" 
+			onChange={this.onChangeDescricao} 
+			value={this.state.descricao}/>
+			
+			<input placeholder="Valor" 
+			type="number" 
+			onChange={this.onChangePreco} 
+			value={this.state.preco}/>
+		
+			<input
+			type="checkbox" 
+			onChange={this.onChangePagamento} 
+			value= "Cartão de Crédito"/>
+			<label>Cartão de Crédito</label>	
+
+			<input
+			type="checkbox" 
+			onChange={this.onChangePagamento} 
+			value= "Cartão de Crédito"/>
+			<label>Cartão de débito</label>	
+
+			<input
+			type="checkbox" 
+			onChange={this.onChangePagamento} 
+			value= "Cartão de Crédito"/>
+			<label>pix</label>	
+		</div>
+	  )
+	}
+  }
+
+
+//Inputs
+  /*<p>1. Nome do serviço oferecido
 			  2. Descrição do serviço
 			  3. preço cobrado
 			  4. tipo de pagamento (opções)
 			  5. input calendario
 			  
-		  </p>
-		</div>
-	  )
-	}
-  }
+		  </p>*/
