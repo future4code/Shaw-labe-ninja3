85b54e17ece473605579d9b7d956db7e6428fda7
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-04-13 10:48:16 -0300

    v

--- src/App.js
+++ src/App.js
@@ -9,40 +9,6 @@ import  Contratacao  from './pages/Contratacao';
 import  AddJob  from './pages/AddJob';
 import  QuemSomos  from './pages/QuemSomos';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- /* 
-foto main junto com  o input de pesquisa
-
- <div>
-		  < img src="https://labenu.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F2a51ad96-d8b7-4df3-bf68-828d473c84ef%2Flabeninjas2.png?table=block&id=c22b9c9a-54d8-4d6e-935e-3931470828dc&spaceId=f97190af-c9c2-4592-9ae2-6311b6b728de&width=740&userId=&cache=v2" />
-		 <h3>pesquisar serviço</h3>
-		  <input placeholder='pesquisar' />
-		  <button>pesquisar</button>
-		  </div>
-		  */
-
 export default class App extends  React.Component {
 
 
@@ -60,3 +26,13 @@ export default class App extends  React.Component {
 	  )
 	}
   }
+/* 
+foto main junto com  o input de pesquisa
+
+ <div>
+		  < img src="https://labenu.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F2a51ad96-d8b7-4df3-bf68-828d473c84ef%2Flabeninjas2.png?table=block&id=c22b9c9a-54d8-4d6e-935e-3931470828dc&spaceId=f97190af-c9c2-4592-9ae2-6311b6b728de&width=740&userId=&cache=v2" />
+		 <h3>pesquisar serviço</h3>
+		  <input placeholder='pesquisar' />
+		  <button>pesquisar</button>
+		  </div>
+		  */
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Axios from 'axios';
 import styled from 'styled-components';
+import App from '../App';
 
 
 export default class Home extends React.Component {
-
-
 	render() {
 	  
 	  return (
