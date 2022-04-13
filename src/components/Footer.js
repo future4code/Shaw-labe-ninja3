297f800e50e6fa484583cb1a8e7fee73242a81e2
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-04-13 09:49:29 -0300

    versao-inicial-home-page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import Axios from 'axios';
-import styled from 'styled-components';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 
-export  default class Footer extends React.Component {
 
 
-	render() {
+export default function TextButtons() {
+	
+
+	
+  return (
+    <Stack direction="row" spacing={8} margin={4}>
+     
+	 <img src="https://cdn-icons-png.flaticon.com/64/409/409334.png" />
+	 <img src ="https://cdn-icons-png.flaticon.com/64/743/743424.png" />
+	 <img src ="https://cdn-icons-png.flaticon.com/64/4500/4500180.png" />
+	 <img src ="https://cdn-icons-png.flaticon.com/64/1149/1149378.png" />
+	 <img src ="https://cdn-icons-png.flaticon.com/64/656/656626.png" />
+	 
 	  
-	  return (
-		<div>
-		  <p>footer</p>
-		</div>
-	  )
-	}
-  }
+	  <Button variant="contained">Chat</Button>
+	  <Button variant="contained">Email</Button>
+	  <Button variant="contained">Quem Somos</Button>
+	  <Button variant="contained" >Faq</Button>
+    </Stack>
+  );
+}
