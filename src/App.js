3655011a92c9807a2b8dd0ad695113657dd45a96
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-04-14 11:37:56 -0300

    versao-03

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import Axios from 'axios';
 import style from 'styled-components';
-import  Carrinho  from './pages/Carrinho';
-import  Header  from './components/Header';
-import  Home  from './pages/Home';
-import  Footer  from './components/Footer';
-import  Contratacao  from './pages/Contratacao';
-import  AddJob  from './pages/AddJob';
-import  QuemSomos  from './pages/QuemSomos';
+import Carrinho from './pages/Carrinho';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import Contratacao from './pages/Contratacao';
+import AddJob from './pages/AddJob';
+import QuemSomos from './pages/QuemSomos';
 
 
-export default class App extends  React.Component {
+export default class App extends React.Component {
+	state = {
+		telaAtual: ""
 
-
-	render() {
-	  
-	  return (
-		<div>
-		  
-		 
-		 <AddJob/>
-		 
-		  
-		</div>
-	  )
 	}
-  }
-/* 
-foto main junto com  o input de pesquisa
+	selecionarPagina = () => {
+		switch (this.state.telaAtual) {
+			case "sejaUmNinja":
+				return <AddJob />
+			case "contrateUmNinja":
+				return <Contratacao />
+			case "quemSomos":
+				return <QuemSomos />
+			default:
+				return <Home />
 
- <div>
-		  < img src="https://labenu.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F2a51ad96-d8b7-4df3-bf68-828d473c84ef%2Flabeninjas2.png?table=block&id=c22b9c9a-54d8-4d6e-935e-3931470828dc&spaceId=f97190af-c9c2-4592-9ae2-6311b6b728de&width=740&userId=&cache=v2" />
-		 <h3>pesquisar serviço</h3>
-		  <input placeholder='pesquisar' />
-		  <button>pesquisar</button>
-		  </div>
-		  */
+		}
+	}
+	irParaPagina = (telaAtual) =>{
+this.setState({telaAtual: "sejaUmNinja"})
+	}
+	irParaPagina2 = (telaAtual) =>{
+		this.setState({telaAtual: "contrateUmNinja"})
+			}
+			irParaPagina3 = (telaAtual) =>{
+				this.setState({telaAtual: "quemSomos"})
+					}
+	render() {
+
+		return (
+			<div>
+				<button></button>
+				{this.selecionarPagina()}
+
+
+			</div>
+		)
+	}
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import styled from 'styled-components'
+const Headerdiv = styled.div`
+border: 3px solid black;
+display:flex;
+background-color:#F1F0FA;
+`
+const HaderA =styled.div`
+background-color: #F1F0FA;
+`
+const ButtonHeader = styled.button`
+background-color:#9584D0;
+margin-top: 4%;
+`
+
+
+
+
+
 export default class Header extends React.Component{
+  state ={
+	  paginas: "app.js"
+  }
+
+
+
 
 	render(){
 		return(
-			<div>
-				a
-			</div>
+			<Headerdiv>
+				<div>
+				<img src="https://labenu.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F2a51ad96-d8b7-4df3-bf68-828d473c84ef%2Flabeninjas2.png?table=block&id=c22b9c9a-54d8-4d6e-935e-3931470828dc&spaceId=f97190af-c9c2-4592-9ae2-6311b6b728de&width=740&userId=&cache=v2 "width="140" height="140"/>
+				</div>
+				<HaderA>
+					<ButtonHeader>Seja um ninja</ButtonHeader>
+					<ButtonHeader>Contrante um ninja</ButtonHeader>
+					<ButtonHeader>Quem somos</ButtonHeader>
+				</HaderA>
+				
+			</Headerdiv>
 		)
 	}
 }
